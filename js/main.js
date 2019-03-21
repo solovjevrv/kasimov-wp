4a1dcbeb3a64b2814ce6c2583263bb81a792aaf3
Roman <solovjevrv@gmail.com>
2019-03-21 16:47:23 +0300

    Сделаны блоки Delivery, Cooperation (без адаптива)

--- js/main.js
+++ js/main.js
@@ -4,3 +4,103 @@ $(document).ready(function(){
       return false;
   });
 });
+
+$(document).ready(function() {  // for Step-2
+  $(".step-2").mouseover(function (){ 
+  $(".step-2 path").css("fill","#EB7F00");
+  $(".circle-2").css("background","#EB7F00");
+  $(".step-1 .line").css("background","#EB7F00");
+    
+   });
+   $(".step-2").mouseout(function (){
+   $(".step-2 path").css("fill","#225378");
+   $(".circle-2").css("background","#225378");
+   $(".step-1 .line").css("background","#225378");
+   });
+  });
+
+  $(document).ready(function() {  // for Step-2, Step-3
+    $(".step-3").mouseover(function (){ 
+    $(".step-2 path").css("fill","#EB7F00");
+    $(".circle-2").css("background","#EB7F00");
+    $(".step-1 .line").css("background","#EB7F00");
+    $(".step-3 path").css("fill","#EB7F00");
+    $(".circle-3").css("background","#EB7F00");
+    $(".step-2 .line").css("background","#EB7F00");
+      
+     });
+     $(".step-3").mouseout(function (){
+     $(".step-2 path").css("fill","#225378");
+     $(".circle-2").css("background","#225378");
+     $(".step-1 .line").css("background","#225378");
+     $(".step-3 path").css("fill","#225378");
+     $(".circle-3").css("background","#225378");
+     $(".step-2 .line").css("background","#225378");
+     });
+    });
+
+
+    $(document).ready(function() {  // for Step-2, Step-3, Step-4
+      $(".step-4").mouseover(function (){ 
+      $(".step-2 path").css("fill","#EB7F00");
+      $(".circle-2").css("background","#EB7F00");
+      $(".step-1 .line").css("background","#EB7F00");
+      $(".step-3 path").css("fill","#EB7F00");
+      $(".circle-3").css("background","#EB7F00");
+      $(".step-2 .line").css("background","#EB7F00");
+      $(".step-4 path").css("fill","#EB7F00");
+      $(".circle-4").css("background","#EB7F00");
+      $(".step-3 .line").css("background","#EB7F00");
+        
+       });
+       $(".step-4").mouseout(function (){
+       $(".step-2 path").css("fill","#225378");
+       $(".circle-2").css("background","#225378");
+       $(".step-1 .line").css("background","#225378");
+       $(".step-3 path").css("fill","#225378");
+       $(".circle-3").css("background","#225378");
+       $(".step-2 .line").css("background","#225378");
+       $(".step-4 path").css("fill","#225378");
+       $(".circle-4").css("background","#225378");
+       $(".step-3 .line").css("background","#225378");
+       });
+      });
+
+      $(document).ready(function() {  // for Step-2, Step-3, Step-4, Step-5
+        $(".step-5").mouseover(function (){ 
+        $(".step-2 path").css("fill","#EB7F00");
+        $(".circle-2").css("background","#EB7F00");
+        $(".step-1 .line").css("background","#EB7F00");
+        $(".step-3 path").css("fill","#EB7F00");
+        $(".circle-3").css("background","#EB7F00");
+        $(".step-2 .line").css("background","#EB7F00");
+        $(".step-4 path").css("fill","#EB7F00");
+        $(".circle-4").css("background","#EB7F00");
+        $(".step-3 .line").css("background","#EB7F00");
+        $(".step-5 path").css("fill","#EB7F00");
+        $(".circle-5").css("background","#EB7F00");
+        $(".step-4 .line").css("background","#EB7F00");
+          
+         });
+         $(".step-5858/").mouseout(function (){
+         $(".step-2 path").css("fill","#225378");
+         $(".circle-2").css("background","#225378");
+         $(".step-1 .line").css("background","#225378");
+         $(".step-3 path").css("fill","#225378");
+         $(".circle-3").css("background","#225378");
+         $(".step-2 .line").css("background","#225378");
+         $(".step-4 path").css("fill","#225378");
+         $(".circle-4").css("background","#225378");
+         $(".step-3 .line").css("background","#225378");
+         $(".step-5 path").css("fill","#225378");
+         $(".circle-5").css("background","#225378");
+         $(".step-4 .line").css("background","#225378");
+         });
+        });
+    
+
+     
+
+
+
+
